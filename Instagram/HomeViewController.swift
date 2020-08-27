ba658bd61b7f4f794297388f7ce76ca8bc6a2829
Shiva Ram Reddy <kathishivaram@gmail.com>
2020-08-28 00:54:33 +0530

    Add tab bar controller and tab icons

---
+++ Instagram/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Shiva Ram Reddy on 28/08/20.
+//  Copyright © 2020 Shiva Ram Reddy. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
