49e303059d096f05034ac7f6a3372ad4d68415a7
Shiva Ram Reddy <kathishivaram@gmail.com>
2020-08-29 14:42:27 +0530

    Setup login view

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -10,21 +10,55 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    struct Constant {
+        
+        static var cornerRadius: CGFloat = 8.0
+    }
+    
     private lazy var userNameField: UITextField = {
         
-        return UITextField()
+        let field = UITextField()
+        field.placeholder = "Username or Email ..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocorrectionType = .no
+        field.autocapitalizationType = .none
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constant.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        return field
     }()
     
     private lazy var passwordField: UITextField = {
         
         let field = UITextField()
         field.isSecureTextEntry = true
+        field.placeholder = "Password..."
+        field.returnKeyType = .continue
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocorrectionType = .no
+        field.autocapitalizationType = .none
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constant.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
         return field
     }()
     
     private lazy var loginButton: UIButton = {
         
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Constant.cornerRadius
+        button.backgroundColor = .systemBlue
+        return button
     }()
     
     private lazy var termsButton: UIButton = {
@@ -39,23 +73,76 @@ class LoginViewController: UIViewController {
     
     private lazy var createAccountButton: UIButton = {
         
-        return UIButton()
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("New User? Create an Account", for: .normal)
+        return button
     }()
     
     private lazy var headerView: UIView = {
         
-        return UIView()
+        let header = UIView(frame: CGRect(x: 0
+            , y: 0
+            , width: self.view.frame.size.width
+            , height: self.view.frame.size.height/3.0))
+        header.clipsToBounds = true
+        header.backgroundColor = .red
+        
+        return header
+    }()
+    
+    private lazy var backgroudView: UIImageView = {
+        
+        let backgroundView = UIImageView(image: UIImage(named: "gradient"))
+        backgroundView.frame = self.headerView.bounds
+        return backgroundView
+    }()
+    
+    private lazy var logoView: UIImageView = {
+        
+        let imageView = UIImageView(image: UIImage(named: "text"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: self.headerView.frame.size.width/4.0
+            , y: self.view.safeAreaInsets.top
+            , width: self.headerView.frame.size.width/2.0
+            , height: self.headerView.frame.size.height - self.view.safeAreaInsets.top)
+        return imageView
     }()
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
         self.addSubviews()
+        self.view.backgroundColor = .systemBackground
+        
+        self.userNameField.delegate = self
+        self.passwordField.delegate = self
     }
     
     override func viewDidLayoutSubviews() {
         
         super.viewDidLayoutSubviews()
+        
+        self.userNameField.frame = CGRect(x: 25
+            , y: self.headerView.frame.origin.y + self.headerView.frame.size.height + 10
+            , width: self.view.frame.size.width - 50
+            , height: 52)
+        
+        self.passwordField.frame = CGRect(x: 25
+        , y: self.userNameField.frame.origin.y + self.userNameField.frame.size.height + 10
+        , width: self.view.frame.size.width - 50
+        , height: 52)
+        
+        self.loginButton.frame = CGRect(x: 25
+        , y: self.passwordField.frame.origin.y + self.passwordField.frame.size.height + 10
+        , width: self.view.frame.size.width - 50
+        , height: 52)
+        
+        self.createAccountButton.frame = CGRect(x: 25
+        , y: self.loginButton.frame.origin.y + self.loginButton.frame.size.height + 10
+        , width: self.view.frame.size.width - 50
+        , height: 52)
+        
     }
     
     
@@ -68,6 +155,8 @@ class LoginViewController: UIViewController {
         self.view.addSubview(self.privacyButton)
         self.view.addSubview(self.createAccountButton)
         self.view.addSubview(headerView)
+        self.headerView.addSubview(self.backgroudView)
+        self.headerView.addSubview(self.logoView)
     }
     
     @objc private func didTapLoginButton() {}
@@ -75,3 +164,5 @@ class LoginViewController: UIViewController {
     @objc private func didTapTermsButton() {}
     @objc private func didTapCreateAccountButton() {}
 }
+
+extension LoginViewController: UITextFieldDelegate {}
