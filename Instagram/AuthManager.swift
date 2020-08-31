bf1ee7accdaa231bfd8f4de0922483e3aad1b76d
Shiva Ram Reddy <kathishivaram@gmail.com>
2020-08-31 23:45:29 +0530

    Add AuthManager and Registration view controller

---
+++ Instagram/AuthManager.swift
@@ -0,0 +1,9 @@
+//
+//  AuthManagger.swift
+//  Instagram
+//
+//  Created by Shiva Ram Reddy on 31/08/20.
+//  Copyright © 2020 Shiva Ram Reddy. All rights reserved.
+//
+
+import Foundation
---
+++ Instagram/RegistrationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationViewController.swift
+//  Instagram
+//
+//  Created by Shiva Ram Reddy on 31/08/20.
+//  Copyright © 2020 Shiva Ram Reddy. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
